00723647da243d63d02128c9a9495605f9d1c265
mushfiq0000 <kazi.mushfiq.ehsan@gmail.com>
2025-11-11 17:27:40 +0600

    add cros and use cors and express json

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = 3000
+app.use(cors())
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Server is running')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`server is listening on port ${port}`)
 })
